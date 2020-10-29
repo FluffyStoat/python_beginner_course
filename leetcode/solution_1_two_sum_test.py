843453e71037cd2562b4f65bc6e49ec65318dbe8
Konstantin Bochkarev <bochkarev@haulmont.com>
2020-10-29 09:03:35 +0400

    (Leetcode) 1. Two Sum
    Start execution

---
+++ leetcode/solution_1_two_sum_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode import solution_1_two_sum
+
+
+class CalcTest(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(solution_1_two_sum.Solution.two_sum([2, 7, 11, 15], 9), [0, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
