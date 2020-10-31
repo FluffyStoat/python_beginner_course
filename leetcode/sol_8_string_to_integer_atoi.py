e04ee2388ce72e050ef74d03c37da64f911e3b60
Konstantin Bochkarev <bkv.developer@gmail.com>
2020-10-31 21:50:49 +0400

    8. String to Integer (atoi) alg has been refactored

--- leetcode/sol_8_string_to_integer_atoi.py
+++ leetcode/sol_8_string_to_integer_atoi.py
@@ -1,5 +1,6 @@
 # 8. String to Integer (atoi)
 
+import re
 # Implement atoi which converts a string to an integer.
 # The function first discards as many whitespace characters as necessary until the first non-whitespace character
 # is found. Then, starting from this character takes an optional initial plus or minus sign followed by as many
@@ -48,70 +49,91 @@
 # Output: -2147483648
 # Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer. Thefore INT_MIN (−231)
 # is returned.
-from typing import List
+from typing import List, Optional, Match, AnyStr
 
 
 class Solution:
     @staticmethod
-    def my_atoi(message: str) -> int:
-        symbols: List[str] = []
+    def atoi_short(s: str) -> int:
+        groups: Optional[Match[AnyStr]] = re.compile("\\s*([+-]?\\d+)").match(s)
+        if groups:
+            v = int(groups.groups()[0])
+            if v < -2 ** 31:
+                return -2 ** 31
+            elif v > 2 ** 31 - 1:
+                return 2 ** 31 - 1
+            return v
+        return 0
 
-        dictionary: str = "-+ .1234567890"
-        int_min: int = pow(-2, 31)
-        int_max: int = pow(2, 31) - 1
+    @staticmethod
+    def atoi(msg: str) -> int:
+        #  : 32 +: 43 -: 45 .: 46
+        #  0: 48 1: 49 2: 50 3: 51 4: 52 5: 53 6: 54 7: 55 8: 56 9: 57
+        # ord(symbol) chr(num)
+        nums: List[int] = [ord(c) for c in msg]
+        sign: int = 1
+        digits: List[int] = []
+        min_val: int = -2 ** 31
+        max_val: int = 2 ** 31 - 1
 
-        message = message.strip()
-        sign: str = None
+        is_digit_found: bool = False
+        is_sign_found: bool = False
 
-        for symbol in message:
-            if symbol in dictionary:
-                if symbol == " " or symbol == ".":
+        for num in nums:
+            if 48 <= num <= 57:
+                is_digit_found = True
+                digits.append(num)
+                continue
+            if num in [43, 45]:
+                if is_sign_found or is_digit_found:
                     break
-
-                if symbol == "-" or symbol == "+":
-                    if sign is not None or len(symbols) > 0:
-                        break
-
-                    if sign is None:
-                        sign = symbol
-                        continue
-                    else:
-                        return 0
-
-                symbols.append(symbol)
-            else:
+                is_sign_found = True
+                sign = -1 if num == 45 else 1
+                continue
+            if num == 32:
+                if is_digit_found or is_sign_found:
+                    break
+                continue
+            if num == 46:
                 break
 
-        if len(symbols) == 0:
+            break
+
+        if len(digits) == 0:
             return 0
 
-        value: int = int(''.join(symbols))
+        result: int = sign * int(''.join([chr(d) for d in digits]))
 
-        if sign == "-":
-            value = value * (-1)
-
-        if value < int_min:
-            value = int_min
-
-        if value > int_max:
-            value = int_max
-
-        return value
+        return result if min_val < result < max_val \
+            else min_val if result < max_val else max_val
 
 
 def main():
     solution: Solution = Solution()
 
-    print(f'"42": {solution.my_atoi("42")}')
-    print(f'"   -42": {solution.my_atoi("   -42")}')
-    print(f'"4193 with words": {solution.my_atoi("4193 with words")}')
-    print(f'"-91283472332": {solution.my_atoi("-91283472332")}')
-    print(f'"-56.73 472332": {solution.my_atoi("-56.73 472332")}')
-    print(f'"+-12": {solution.my_atoi("+-12")}')
-    print(f'"++12": {solution.my_atoi("++12")}')
-    print(f'"--12": {solution.my_atoi("--12")}')
-    print(f'"00000-42a1234": {solution.my_atoi("00000-42a1234")}')
-    print(f'"-5-": {solution.my_atoi("-5-")}')
+    print('Slow version')
+    print(f'"42": {solution.atoi("42")}')
+    print(f'"   -42": {solution.atoi("   -42")}')
+    print(f'"4193 with words": {solution.atoi("4193 with words")}')
+    print(f'"-91283472332": {solution.atoi("-91283472332")}')
+    print(f'"-56.73 472332": {solution.atoi("-56.73 472332")}')
+    print(f'"+-12": {solution.atoi("+-12")}')
+    print(f'"++12": {solution.atoi("++12")}')
+    print(f'"--12": {solution.atoi("--12")}')
+    print(f'"00000-42a1234": {solution.atoi("00000-42a1234")}')
+    print(f'"-5-": {solution.atoi("-5-")}')
+
+    print('Short version')
+    print(f'"42": {solution.atoi_short("42")}')
+    print(f'"   -42": {solution.atoi_short("   -42")}')
+    print(f'"4193 with words": {solution.atoi_short("4193 with words")}')
+    print(f'"-91283472332": {solution.atoi_short("-91283472332")}')
+    print(f'"-56.73 472332": {solution.atoi_short("-56.73 472332")}')
+    print(f'"+-12": {solution.atoi_short("+-12")}')
+    print(f'"++12": {solution.atoi_short("++12")}')
+    print(f'"--12": {solution.atoi_short("--12")}')
+    print(f'"00000-42a1234": {solution.atoi_short("00000-42a1234")}')
+    print(f'"-5-": {solution.atoi_short("-5-")}')
 
 
 if __name__ == "__main__":
