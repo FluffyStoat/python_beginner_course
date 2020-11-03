62228d8179869206cd0bf2d89303af5f99f35af8
Konstantin Bochkarev <bkv.developer@gmail.com>
2020-11-03 17:49:45 +0400

    thm python [Medium] Gotta Catch em All: temporal version

--- thm/gotta_catch_em_all.py
+++ thm/gotta_catch_em_all.py
@@ -22,51 +22,88 @@
 # Send an operation
 # View response and continue
 import re
-import socket
-from typing import Optional
 import time
+from typing import Optional
 import requests
 from requests import Response
 
-HOST: str = '10.10.149.102'  # Standard loopback interface address (localhost)
+HOST: str = '10.10.127.201'  # Standard loopback interface address (localhost)
 PORT: int = 3010  # Port to listen on (non-privileged ports are > 1023)
-URL: str = f'http://{HOST}:{PORT}'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
 }
 
 
+def parse_command(cmd: str, amount: float) -> (int, int):
+    command: [str] = cmd.split()
+
+    print(f'command: {command[0]} {command[1]} {command[2]}')
+
+    if command[0] == 'minus':
+        amount = amount - int(command[1])
+    elif command[0] == 'add':
+        amount = amount + int(command[1])
+    elif command[0] == 'divide':
+        amount = amount / float(command[1])
+    elif command[0] == 'multiply':
+        amount = amount * int(command[1])
+
+    next_port: int = int(command[2])
+    print(f'amount: {amount}')
+
+    return amount, next_port
+
+
 def main():
-    response: Response = requests.get(URL, headers=headers)
+    response: Response = requests.get(f'http://{HOST}:{PORT}', headers=headers)
+    response.close()
     print(f'{response.text}')
+    time.sleep(1)
 
     found: Optional = re.compile('":([0-9]+)"').search(response.text)
     if found:
         start_port = int(found.group(1))
         print(f"start_port: {start_port}")
 
-        next_port: int = start_port
+        first_response: Response = requests.get(f'http://{HOST}:{start_port}')
+        first_response.close()
+        cmd: str = first_response.text
+        time.sleep(1)
 
-        while True:
-            time.sleep(1)
-            resp: Response = requests.get(f'http://{HOST}:{next_port}', headers=headers)
+        print(f"first_command: {cmd}")
 
-            if resp.text == 'STOP':
-                break
+        parsed_command: [str] = parse_command(first_response.text, 0.0)
 
-            command: [str] = resp.text.split()
-            print(f'command: {command[0]} {command[1]} {command[2]}')
-            if command[0] == 'minus':
-                start_port = start_port - int(command[1])
-            elif command[0] == 'add':
-                start_port = start_port + int(command[1])
-            elif command[0] == 'divide':
-                start_port = start_port + int(command[1])
+        print(f"parsed_command[0]: {parsed_command[0]} parsed_command[1]: {parsed_command[1]}")
 
-            next_port = int(command[2])
-            print(f"result = {start_port}")
-            print(f"{next_port}")
+        amount: float = parsed_command[0]
+        next_port: int = parsed_command[1]
+
+        while cmd != "STOP" or next_port != 9765:
+            try:
+                time.sleep(10)
+                cmd_response: Response = requests.get(f'http://{HOST}:{next_port}')
+                cmd_response.close()
+                cmd = cmd_response.text
+                print(f"resp: {cmd_response.text}")
+
+                new_cmd: [str] = parse_command(cmd, amount)
+                amount = new_cmd[0]
+                next_port = new_cmd[1]
+            except requests.exceptions.ConnectionError:
+                print("Connection error")
+                raise
+
+        # while cmd != "STOP" or next_port != 9765:
+        #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #         s.connect((HOST, next_port))
+        #         s.send(f"{amount}".encode("Utf-8"))
+        #         data = s.recv(1024)
+        #         print(f'Data: {repr(data)}')
+
+
+
 
     else:
         print("NOT_FOUND")
