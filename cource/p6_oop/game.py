3242d826a375325356f83634413e2642251056ae
Konstantin Bochkarev <bkv.developer@gmail.com>
2020-11-24 22:44:03 +0400

    6 part of basic course

--- cource/p6_oop/game.py
+++ cource/p6_oop/game.py
@@ -12,37 +12,59 @@
 # там крестик или нолик.
 #
 # Для упрощения - тот кто ходит первым - ставит крестик.
+from typing import Union
 
 import pygame
 
 BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+SEA_GREEN = [46, 139, 87]
+STEEL_BLUE = [70, 130, 180]
+GREEN = [0, 128, 0]
+YELLOW_GREEN = [154, 205, 50]
 
 
 def main():
     pygame.init()
 
     # Set up the drawing window
-    screen = pygame.display.set_mode([600, 600])
+    screen = pygame.display.set_mode([800, 800])
     pygame.display.set_caption("Крестики-нолики")
 
-    f1 = pygame.font.Font(None, 36)
-    text1 = f1.render('Hello Привет', True, (180, 0, 0))
+    font_36 = pygame.font.Font(None, 36)
+
+    text_player1: Union = font_36.render('Игрок 1 (X)', True, (0, 80, 0))
+    text_player2: Union = font_36.render('Игрок 2 (O)', True, (80, 80, 0))
+
+    text_start: Union = font_36.render("Старт", True, WHITE)
 
     # Run until the user asks to quit
     running = True
 
     while running:
+        mouse = pygame.mouse.get_pos()
 
         # Did the user click the window close button?
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Todo: add calculation of field part
+                print(f"mouse click: {mouse[0]}:{mouse[1]}")
 
         # Fill the background with white
         screen.fill((255, 255, 255))
 
-        screen.blit(text1, (10, 50))
+        draw_menu(screen, text_player1, text_player2)
         draw_field(screen)
+
+        # Todo: Should extract to function
+        if 330 < mouse[0] < 470 and 730 < mouse[1] < 770:
+            pygame.draw.rect(screen, GREEN, [330, 730, 140, 40])
+        else:
+            pygame.draw.rect(screen, YELLOW_GREEN, [330, 730, 140, 40])
+
+        screen.blit(text_start, (365, 736))
 
         # Flip the display
         pygame.display.flip()
@@ -51,11 +73,18 @@ def main():
     pygame.quit()
 
 
+# Todo: refactoring draw menu with initialization
+def draw_menu(screen: pygame.Surface, text_player1: Union, text_player2: Union):
+    screen.blit(text_player1, (50, 50))
+    screen.blit(text_player2, (600, 50))
+
+
+# Todo: refactor creation with array
 def draw_field(screen: pygame.Surface):
-    pygame.draw.line(screen, BLACK, [200, 0], [200, 600], 4)
-    pygame.draw.line(screen, BLACK, [400, 0], [400, 600], 4)
-    pygame.draw.line(screen, BLACK, [0, 200], [600, 200], 4)
-    pygame.draw.line(screen, BLACK, [0, 400], [600, 400], 4)
+    pygame.draw.line(screen, BLACK, [300, 100], [300, 700], 4)
+    pygame.draw.line(screen, BLACK, [500, 100], [500, 700], 4)
+    pygame.draw.line(screen, BLACK, [100, 300], [700, 300], 4)
+    pygame.draw.line(screen, BLACK, [100, 500], [700, 500], 4)
 
 
 if __name__ == "__main__":
